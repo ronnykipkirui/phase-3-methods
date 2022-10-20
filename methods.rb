da648859a39626510068a92b851aea6212829f61
Ronny <ronnybet41@gmail.com>
2022-10-20 13:03:59 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,20 +1,19 @@
-def greet_programmers
+def greet_programmer 
   puts "Hello, programmer!"
-  end
+end
 
 
-  def greet name
-    puts "Hello, #{name}"
-  end
+def greet name
+puts "Hello, #{name}!"
+end
 
-  def greet_with_default(name="Hello, programmer!")
-    puts "Hello, #{name}"
-  end
+def greet_with_default(name="Hello, programmer!")
+  puts "Hello, #{name}!"
+end
 
 def add(a,b)
   a+b
 end
-
 
 def halve(num)
   num.class == Integer ? num/2 : nil
